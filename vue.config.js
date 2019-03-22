5ca6da9fa3700397eff247ef3a08340baa6905fe
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-03-22 12:17:14 -0400

    fix: importing styles for material components

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,24 @@
 const path = require('path')
 
 module.exports = {
-  // Automatically import base styles
+  // Automatically import base styles everywhere
   pluginOptions: {
     'style-resources-loader': {
       patterns: [path.resolve(__dirname, 'src/styles/*.scss')],
       preProcessor: 'scss'
+    }
+  },
+  /*
+   * Importing MDC component styles causes issues with our Webpack config, which this fixes:
+   * https://github.com/material-components/material-components-web/issues/351#issuecomment-318981488
+   */
+  css: {
+    loaderOptions: {
+      sass: {
+        includePaths: ['node_modules', 'node_modules/@material/*'].map(d =>
+          path.join(__dirname, d)
+        )
+      }
     }
   }
 }
