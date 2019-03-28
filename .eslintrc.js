22e8dd8520b60643a8431f9dc9ca62067b809042
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-03-28 13:03:04 -0400

    feat: add electron support
    - add main process script to handle window management
    - remove babel (since Electron natively supports everything we need)
    - stricter eslint config
    - begin migration to typescript
    - add rules to vue.config.js to fix issues with native modules in Electron
    - remove favicon, since it's unnecessary
    - remove unnecessary boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
+
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    parser: '@typescript-eslint/parser'
+  },
+
+  extends: ['plugin:vue/recommended', '@vue/prettier', '@vue/typescript']
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const path = require('path')
+const glob = require('fast-glob')
+const _ = require('lodash')
+const webpack = require('webpack')
+
+// TODO Better solution?
+let skips = ['electron', 'npm']
+let externals = _.uniq(
+  glob
+    .sync(
+      ['gyp'].map(v => {
+        return `node_modules/**/*.${v}`
+      })
+    )
+    .map(v => v.split('/')[1])
+    .filter(v => {
+      return skips.indexOf(v) == -1
+    })
+).sort()
 
 module.exports = {
   // Automatically import base styles everywhere
@@ -6,7 +24,19 @@ module.exports = {
     'style-resources-loader': {
       patterns: [path.resolve(__dirname, 'src/styles/*.scss')],
       preProcessor: 'scss'
+    },
+    electronBuilder: {
+      // List native deps here if they don't work
+      externals
     }
+  },
+  // TODO Is this necessary?
+  configureWebpack(config) {
+    config.plugins.push(new webpack.ExternalsPlugin('commonjs', externals))
+    config.resolve.alias.ws = path.resolve(
+      __dirname,
+      'node_modules/ws/index.js'
+    )
   },
   /*
    * Importing MDC component styles causes issues with our Webpack config, which this fixes:
